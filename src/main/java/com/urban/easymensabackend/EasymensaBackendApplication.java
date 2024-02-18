10b3473ac665df69c8dad07a0ba2c704c3653c41
Urban701 <pekmez1507@gmail.com>
2024-02-18 17:25:34 +0100

    init project

---
+++ src/main/java/com/urban/easymensabackend/EasymensaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.urban.easymensabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymensaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymensaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urban/easymensabackend/EasymensaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urban.easymensabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymensaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
